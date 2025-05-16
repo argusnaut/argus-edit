fcb0e48b020c54fef564fa7e89e95357a8be0778
argusnaut <argusvi@outlook.com>
2025-05-16 16:45:07 -0300

    Search prompt inclusion, no functionality

--- src/editor/command.rs
+++ src/editor/command.rs
@@ -84,6 +84,7 @@ pub enum System {
     Resize(Size),
     Quit,
     Dismiss,
+    Search,
 }
 
 impl TryFrom<KeyEvent> for System {
@@ -98,6 +99,7 @@ impl TryFrom<KeyEvent> for System {
             match code {
                 Char('q') => Ok(Self::Quit),
                 Char('s') => Ok(Self::Save),
+                Char('f') => Ok(Self::Search),
                 _ => Err(format!("Unsupported CONTROL+{code:?} combination")),
             }
         } else if modifiers == KeyModifiers::NONE && matches!(code, KeyCode::Esc) {
